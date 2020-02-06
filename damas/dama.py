6d7835ec3768218ee5e685eb3b6cfd7b4602fa9f
Leo Núñez <ljng97@gmail.com>
2020-02-06 16:08:33 -0400

    Agregamos clase Tablero

--- damas/dama.py
+++ damas/dama.py
@@ -28,30 +28,89 @@ Debe hacer provecho del uso de GIT & Github.
 """
 
 class Tablero:
-    def __init__(self):
-        
+    def __init__(self, xCord, yCord):
+        self.xCord = xCord
+        self.yCord = yCord     
+
+    def imprimirTablero(self):
+        self.horizontal = "| A | B | C | D | E | F | G | H |"
+        print(self.horizontal, "\n")
+
+        self.count = 0 
+        for t in range(64):
+            print('|', end=' - ')
+            self.count += 1
+            if self.count == 8:
+                print('|', end='\n')
+                self.count = 0
+
+
+
+class Juego:
+    def __init__(self):      
         self.tablero = [
-            ['O', '-', 'O', '-', 'O', '-', 'O', '-'],
-            ['-', 'O', '-', 'O', '-', 'O', '-', 'O'],
-            ['O', '-', 'O', '-', 'O', '-', 'O', '-'],
-            ['-', '-', '-', '-', '-', '-', '-', '-'],
-            ['-', '-', '-', '-', '-', '-', '-', '-'],
-            ['-', 'X', '-', 'X', '-', 'X', '-', 'X'],
-            ['X', '-', 'X', '-', 'X', '-', 'X', '-'],
-            ['-', 'X', '-', 'X', '-', 'X', '-', 'X']]
+            [' ', '-', ' ', '-', ' ', '-', ' ', '-'],
+            ['-', ' ', '-', ' ', '-', ' ', '-', ' '],
+            [' ', '-', ' ', '-', ' ', '-', ' ', '-'],
+            ['-', ' ', '-', ' ', '-', ' ', '-', ' '],
+            [' ', '-', ' ', '-', ' ', '-', ' ', '-'],
+            ['-', ' ', '-', ' ', '-', ' ', '-', ' '],
+            [' ', '-', ' ', '-', ' ', '-', ' ', '-'],
+            ['-', ' ', '-', ' ', '-', ' ', '-', ' ']]
+
+    def valores(self):
+        self.horizontal = "    A |  B |  C |  D |  E |  F |  G |  H |"
+        print(self.horizontal, "\n")
+
+        for i in range(1,8,2):
+            self.tablero[0].insert(i, 'b')
+
+        for i in range(0,8,2):
+            self.tablero[1].insert(i, 'b')
+            
+        for i in range(1,8,2):
+            self.tablero[2].insert(i, 'b')
+
+        for i in range(0,8,2):
+            self.tablero[7].insert(i, 'n')
+
+        for i in range(1,8,2):
+            self.tablero[6].insert(i, 'n')
+
+        for i in range(0,8,2):
+            self.tablero[5].insert(i, 'n')
+
+        for i, k in enumerate( self.tablero ):
+            print( i, k)
+
+
+    def posicion(self):
+        self.tablero[0] = {1: 'b', 2: '-', 3: 'b', 4: '-', 5: 'b', 6: '-', 7: 'b', 8: '-' }
+        self.tablero[1] = []
+        self.tablero[2] = []
+        self.tablero[3] = []
+        self.tablero[4] = []
+        self.tablero[5] = []
+        self.tablero[6] = []
+        self.tablero[7] = []
+                        
+# t = Juego()
+# t.valores()
+# # t.posicion()
+
+# print("  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |")
+# for i in range(0,8):
+#             print("-"*32)
+#             print(chr(i+97),end="| ")
+#             for j in range(0,8):
+#                 item = ((i,j)," ")
+#                 print(str(item)+' |', end = " ")
+#             print()
+#             print("-"*32)
 
 
 
-t = Tablero()
-
-print( t.tablero[7] )
-print( t.tablero[6] )
-print( t.tablero[5] )
-print( t.tablero[4] )
-print( t.tablero[3] )
-print( t.tablero[2] )
-print( t.tablero[1] )
-print( t.tablero[0] )
-
-# print( t.tablero[2][4])
-# print( t.tablero[3][5])
+# def startBoard():
+#         colorPos = [[None] * 8 for i in range(8)] # [[None]*8]*8
+#         print(colorPos)
+# startBoard()
